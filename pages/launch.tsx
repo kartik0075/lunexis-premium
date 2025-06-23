5769ac033c0d3445813f7c1634fa14b64324e241
Kartik Yadav <kartikyadav9087@gmail.com>
2025-06-23 22:46:34 +0530

    Removed legacy pages/ folder to fix Vercel build

--- pages/launch.tsx
+++
@@ -1,7 +0,0 @@
-export default function Launch() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-black text-white animate-fadeIn">
-      <h1 className="text-4xl">Welcome to Lunexis 🌌</h1>
-    </div>
-  );
-}
